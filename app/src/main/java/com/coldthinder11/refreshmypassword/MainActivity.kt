6d529e55a7d7bce51b8617edd344d6b6814f274c
ColdThunder11 <lslyj27761@gmail.com>
2022-04-14 23:02:33 +0800

    auto set router password while possible

--- app/src/main/java/com/coldthinder11/refreshmypassword/MainActivity.kt
+++ app/src/main/java/com/coldthinder11/refreshmypassword/MainActivity.kt
@@ -47,6 +47,8 @@ data class SaveDataJson(
 
 class MainActivity : AppCompatActivity() {
     var routerIp :String? = null;
+    var is_exp = true;
+    var wifi_ready = false;
     var exp_time :String? = null;
 
     fun getIpAddr(ip:Int): InetAddress?{
@@ -88,9 +90,13 @@ class MainActivity : AppCompatActivity() {
                             findViewById<TextView>(R.id.smsStatusText).text = ("密码:${password}\n过期时间:${expired_time}")
                             val date = SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(expired_time);
                             if(date < Date()){
+                                is_exp = true;
                                 Toast.makeText(this,"密码已过期",Toast.LENGTH_SHORT).show();
                             }
-                            else Toast.makeText(this,"密码未过期",Toast.LENGTH_SHORT).show();
+                            else {
+                                is_exp = false;
+                                Toast.makeText(this,"密码未过期",Toast.LENGTH_SHORT).show();
+                            }
                             return
                         }
                     }
@@ -159,7 +165,10 @@ class MainActivity : AppCompatActivity() {
             val resp = client.newCall(request).execute()
             val res = resp.body?.string()
             if(res != null){
-                if(res.indexOf("LuCI") >= 0) luciStatus = "成功检测到LuCI"
+                if(res.indexOf("LuCI") >= 0) {
+                    wifi_ready = true;
+                    luciStatus = "成功检测到LuCI";
+                }
                 else luciStatus = "未能正确检测到LuCI，调用可能发生错误"
             }
             else luciStatus = "未能正确检测到LuCI，调用可能发生错误"
@@ -171,6 +180,108 @@ class MainActivity : AppCompatActivity() {
         wifiStatus.text = "SSID:${ssid}\nGateway:${routerIp}\n${luciStatus}";
     }
 
+    fun setPassword2Router(){
+        val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
+        val cookieStore: HashMap<String, List<Cookie>> = HashMap()
+        val client = OkHttpClient.Builder()
+            .cookieJar(object : CookieJar {
+                override fun saveFromResponse(httpUrl: HttpUrl, list: List<Cookie>) {
+                    cookieStore[httpUrl.host] = list
+                }
+                override fun loadForRequest(httpUrl: HttpUrl): List<Cookie> {
+                    val cookies = cookieStore[httpUrl.host]
+                    return cookies ?: ArrayList()
+                }
+            })
+            .build()
+        //Login
+        val body = FormBody.Builder().add("luci_username",findViewById<EditText>(R.id.userNameTextInput).text.toString())
+            .add("luci_password",findViewById<EditText>(R.id.passwordInputText).text.toString()).build()
+        var request = Request.Builder()
+            .url("http://${routerIp}/cgi-bin/luci/")
+            .post(body)
+            .build()
+        try {
+            val resp = client.newCall(request).execute()
+            if (resp.code == 320 || resp.code == 200){
+                //Toast.makeText(this,"认证成功，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this,"认证错误，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        catch (e:java.lang.Exception){
+            Toast.makeText(this,"网络错误",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        request = Request.Builder()
+            .url("http://${routerIp}/cgi-bin/luci/admin/network/network/${findViewById<EditText>(R.id.interfaceInputText).text.toString()}")
+            .get()
+            .build()
+        try {
+            val resp = client.newCall(request).execute()
+            if (resp.code == 200){
+                val content = resp.body?.string();
+                val target_str = "name=\"token\" value=\"";
+                if(content != null){
+                    val token_index = content.indexOf(target_str)
+                    if(token_index >=0){
+                        val end_index = content.indexOf("\"",token_index+ target_str.length)
+                        val token_str = content.substring(token_index+ target_str.length,end_index)
+                        var body_builder = FormBody.Builder();
+                        val post_template = findViewById<EditText>(R.id.payloadInputText).text.toString().trim()
+                        val post_lines = post_template.lines()
+                        for (line in post_lines){
+                            val key = line.split(":")[0].trim()
+                            var value = line.split(":")[1].trim()
+                            when(key){
+                                "token" -> value = token_str
+                                "cbid.network.l2tp.password" -> value = findViewById<EditText>(R.id.l2tpPasswordInputText).text.toString()
+                            }
+                            body_builder = body_builder.add(key,value)
+                        }
+                        val req_body = body_builder.build()
+                        val request = Request.Builder()
+                            .url("http://${routerIp}/cgi-bin/luci/admin/network/network/${findViewById<EditText>(R.id.interfaceInputText).text}")
+                            .post(req_body)
+                            .build()
+                        try {
+                            val resp = client.newCall(request).execute()
+                            if (resp.code == 200){
+                                //懒得检查调用是否成功了
+                                val body = FormBody.Builder().add("token",token_str).build()
+                                val request = Request.Builder()
+                                    .url("http://${routerIp}/cgi-bin/luci/servicectl/restart/network,wireless,firewall")
+                                    .post(body)
+                                    .build()
+                                val resp = client.newCall(request).execute()
+                                Toast.makeText(this,"密码设置成功",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(this,"认证错误，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+                        catch (e:java.lang.Exception){
+                            Toast.makeText(this,"网络错误",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                    else throw java.lang.Exception()
+                }
+                else throw java.lang.Exception()
+            }
+            else{
+                Toast.makeText(this,"认证错误，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        catch (e:java.lang.Exception){
+            Toast.makeText(this,"网络错误",Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -200,115 +311,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         ReadConfig();
-
         readWifiStatus();
-
         ReadPasswordSMS();
+        if(!is_exp && wifi_ready){
+            setPassword2Router();
+        }
 
         findViewById<Button>(R.id.saveDataButton).setOnClickListener {
             saveInfo();
         };
         findViewById<Button>(R.id.setPasswordButton).setOnClickListener {
-            val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
-            val cookieStore: HashMap<String, List<Cookie>> = HashMap()
-            val client = OkHttpClient.Builder()
-                .cookieJar(object : CookieJar {
-                    override fun saveFromResponse(httpUrl: HttpUrl, list: List<Cookie>) {
-                        cookieStore[httpUrl.host] = list
-                    }
-                    override fun loadForRequest(httpUrl: HttpUrl): List<Cookie> {
-                        val cookies = cookieStore[httpUrl.host]
-                        return cookies ?: ArrayList()
-                    }
-                })
-                .build()
-            //Login
-            val body = FormBody.Builder().add("luci_username",findViewById<EditText>(R.id.userNameTextInput).text.toString())
-                .add("luci_password",findViewById<EditText>(R.id.passwordInputText).text.toString()).build()
-            var request = Request.Builder()
-                .url("http://${routerIp}/cgi-bin/luci/")
-                .post(body)
-                .build()
-            try {
-                val resp = client.newCall(request).execute()
-                if (resp.code == 320 || resp.code == 200){
-                    //Toast.makeText(this,"认证成功，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(this,"认证错误，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
-                    return@setOnClickListener;
-                }
-            }
-            catch (e:java.lang.Exception){
-                Toast.makeText(this,"网络错误",Toast.LENGTH_SHORT).show();
-                return@setOnClickListener;
-            }
-            request = Request.Builder()
-                .url("http://${routerIp}/cgi-bin/luci/admin/network/network/${findViewById<EditText>(R.id.interfaceInputText).text.toString()}")
-                .get()
-                .build()
-            try {
-                val resp = client.newCall(request).execute()
-                if (resp.code == 200){
-                    val content = resp.body?.string();
-                    val target_str = "name=\"token\" value=\"";
-                    if(content != null){
-                        val token_index = content.indexOf(target_str)
-                        if(token_index >=0){
-                            val end_index = content.indexOf("\"",token_index+ target_str.length)
-                            val token_str = content.substring(token_index+ target_str.length,end_index)
-                            var body_builder = FormBody.Builder();
-                            val post_template = findViewById<EditText>(R.id.payloadInputText).text.toString().trim()
-                            val post_lines = post_template.lines()
-                            for (line in post_lines){
-                                val key = line.split(":")[0].trim()
-                                var value = line.split(":")[1].trim()
-                                when(key){
-                                    "token" -> value = token_str
-                                    "cbid.network.l2tp.password" -> value = findViewById<EditText>(R.id.l2tpPasswordInputText).text.toString()
-                                }
-                                body_builder = body_builder.add(key,value)
-                            }
-                            val req_body = body_builder.build()
-                            val request = Request.Builder()
-                                .url("http://${routerIp}/cgi-bin/luci/admin/network/network/${findViewById<EditText>(R.id.interfaceInputText).text}")
-                                .post(req_body)
-                                .build()
-                            try {
-                                val resp = client.newCall(request).execute()
-                                if (resp.code == 200){
-                                    //懒得检查调用是否成功了
-                                    val body = FormBody.Builder().add("token",token_str).build()
-                                    val request = Request.Builder()
-                                        .url("http://${routerIp}/cgi-bin/luci/servicectl/restart/network,wireless,firewall")
-                                        .post(body)
-                                        .build()
-                                    val resp = client.newCall(request).execute()
-                                    Toast.makeText(this,"密码设置成功",Toast.LENGTH_SHORT).show();
-                                }
-                                else{
-                                    Toast.makeText(this,"认证错误，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
-                                    return@setOnClickListener
-                                }
-                            }
-                            catch (e:java.lang.Exception){
-                                Toast.makeText(this,"网络错误",Toast.LENGTH_SHORT).show();
-                                return@setOnClickListener;
-                            }
-                        }
-                        else throw java.lang.Exception()
-                    }
-                    else throw java.lang.Exception()
-                }
-                else{
-                    Toast.makeText(this,"认证错误，HTTP ${resp.code}",Toast.LENGTH_SHORT).show();
-                    return@setOnClickListener;
-                }
-            }
-            catch (e:java.lang.Exception){
-                Toast.makeText(this,"网络错误",Toast.LENGTH_SHORT).show();
-                return@setOnClickListener;
-            }
+            setPassword2Router();
         };
         findViewById<Button>(R.id.rereadButton).setOnClickListener{
             readWifiStatus();
